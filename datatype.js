0845a098f0d5795b13423c13de70cde917a261ae
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-07 13:54:04 +0530

    commit folder

--- datatype.js
+++ datatype.js
@@ -1,2 +1,9 @@
-let x = 10;
-var y = 67;
+//multiple line declaration
+var x;
+x = 10; //initialization
+
+//single line declaration+initializaion
+var x = 10;
+
+//single line multiple declaration+initialization
+var f =98, g=68;
