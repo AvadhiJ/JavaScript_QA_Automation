93e3c51eeee0af82d7fb37e2f16d0ec63bc2caba
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-07 19:04:11 +0530

    commit

---
+++ Basicsofjavascript/Conditional.js
@@ -0,0 +1,29 @@
+/**if condition */
+// let age = 45;
+
+// if(age<=38)
+// {
+//     console.log("eligible for increment");
+// }
+
+/**if else condition */
+// let numb = 87;
+// if (numb%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+/**else if condition */
+// let a=10, b=54,c=8;
+
+// if(a>b && a>c){
+//  console.log("a is largest");
+// }
+// else if(b>a && b>c){
+//     console.log("b is largest");
+// }
+// else{
+//     console.log("c is largest");
+// }
