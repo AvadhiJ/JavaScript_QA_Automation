b58cbe587887b8731d772c0dcba194b3daa307ce
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-08 12:43:49 +0530

    commit loop

---
+++ Basicsofjavascript/loop.js
@@ -0,0 +1,32 @@
+/**for loop = initialization,conditon and increment/decrement */
+// for(let i=1;i<=10;i++){
+// console.log(i);   // 1 2 3 ....10
+
+// }
+
+// for(i=10;i>0;i--){
+//     console.log(i); //10 9 8 7 .....1
+// }
+
+/**while loop  */
+// let i =1;
+// while(i<=10){   //1 2 3 .....10
+
+//     console.log(i);
+//     i=i+1;
+// }
+
+// let i=2;
+// while(i<=10){
+//     console.log(i);
+//     i=i+2;
+// }
+
+/**do while loop */
+// let i=1;
+//  do{
+//     console.log(i);
+//     i++;
+//  }
+//  while(i<=10);
+
