79b9cd14a0a87596d35b3e5b58d4172546d3d091
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-09 12:39:14 +0530

    commit map

---
+++ Functions/Map.js
@@ -0,0 +1,17 @@
+/**Map function = On iterating a map object returns the key, and value pair in the same order as inserted  */
+
+// const arr= [2,4,6,8,10];
+
+// function double(x){
+//   return  x*2;                         //basic map syntax using function
+// }
+
+// const output = arr.map(double);
+// console.log(output);
+
+// const arr= [2,4,6,8,10];
+
+//                                                //basic map syntax using arrowfunction  (binary of the arr map)
+// const output = arr.map((x)=> x.toString(2));
+// console.log(output);
+
