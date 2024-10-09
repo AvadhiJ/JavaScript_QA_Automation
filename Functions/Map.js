f9925e12ee39ce3de7ec324b62f604f8be8a0d3a
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-09 13:48:21 +0530

    commit map

--- Functions/Map.js
+++ Functions/Map.js
@@ -15,3 +15,18 @@
 // const output = arr.map((x)=> x.toString(2));
 // console.log(output);
 
+/**filter */
+// const arr = [1,3,5,6,8,9,11]
+// const res = arr.filter((d)=> d%2);
+// console.log(res);   // filter out the odd number, here we have used arrow function
+
+/**reduce = used at a place where you have take all the elements of the array and come up with a single value */
+
+//find the sum number inside the array
+ 
+const arr = [1,5,3,6,90]
+const res = arr.reduce(function(acc,curr ){
+    acc=acc+curr;
+    return acc;
+},0);
+console.log(res);
