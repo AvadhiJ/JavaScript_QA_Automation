400bada037bf7d609c9f64668e37485905f671e5
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-10 12:30:29 +0530

    commit dom

---
+++ DOM/EventListeners.js
@@ -0,0 +1,33 @@
+// <!-- <!DOCTYPE html>
+// <html>
+// <body>
+
+// <h2>JavaScript addEventListener()</h2>
+
+// <p>This example uses the addEventListener() method to attach a click event to a button.</p>
+
+// <button id="myBtn">Try it</button>
+
+// <p id="demo"></p>
+
+// <script>
+// document.getElementById("myBtn").addEventListener("click", displayDate); // we have used getElementById() for selecting the button
+
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
+// </script>
+
+// </body>
+// </html>  -->
+
+/**The key property returns the key that was pressed when the event occured.
+
+The key property is read-only. */
+
+// event.key
+let key = event.key;
+
+if (key == "a" || key == "A") {
+  let text = "You pressed the 'A' key!";
+}
