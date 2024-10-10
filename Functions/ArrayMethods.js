a59bdf77899ac73e603d8b3efeb11657e2f72048
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-10 12:28:31 +0530

    commit dom

--- Functions/ArrayMethods.js
+++ Functions/ArrayMethods.js
@@ -1,25 +1,25 @@
 /**push method = Adding new elements such as some numbers and some string values to the end of the array using the push() method. */
 
-const arr = [3,4,5,6,7,8];
- arr.push(1,20);
- console.log(arr);
+// const arr = [3,4,5,6,7,8];
+//  arr.push(1,20);
+//  console.log(arr);
 
- /**pop method */
- // Declare and initialize array
-let numArr = [20, 30, 40, 50];
+//  /**pop method */
+//  // Declare and initialize array
+// let numArr = [20, 30, 40, 50];
 
-// Removing elements from end 
-// of an array
-numArr.pop();
+// // Removing elements from end 
+// // of an array
+// numArr.pop();
 
-console.log(numArr);
+// console.log(numArr);
 
 
-// Declare and initialize array
-let strArr = ["amit", "sumit", "anil"];
+// // Declare and initialize array
+// let strArr = ["amit", "sumit", "anil"];
 
-// Removing elements from end 
-// of an array
-strArr.pop();
+// // Removing elements from end 
+// // of an array
+// strArr.pop();
 
-console.log(strArr);
+// console.log(strArr);
