a18e4683a8c72c4b87eaee9e7c22a0e44326ca9f
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-14 11:16:50 +0530

    commit promises

---
+++ ES6/Promises.js
@@ -0,0 +1,18 @@
+/**To handle a resolved promise, use the .then() method, and for a rejected promise, use .catch(). To execute code after promise handling, use .finally(), ensuring the code within runs regardless of the promise state. */
+// const promise = new Promise(function (resolve, reject) {
+//     const string1 = "Avadhi";
+//     const string2 = "Jain";
+//     if (string1 === string2) {
+//         resolve();
+//     } else {
+//         reject();
+//     }
+// });
+
+// promise
+//     .then(function () {
+//         console.log("Promise resolved successfully");
+//     })
+//     .catch(function () {
+//         console.log("Promise is rejected");
+//     });
--- ES6/TemplateLiterals.js
+++ ES6/TemplateLiterals.js
@@ -4,5 +4,23 @@ if (key == "a" || key == "A") {
   let text = "You pressed the 'A' key!";
 } */
 
-  const name1= 'Avadhi';
-  console.log(`My name is ${name1}`);
+//   const name1= 'Avadhi';
+//   console.log(`My name is ${name1}`);
+
+
+  /**Object and Array Desctructuring */
+  //Destructing in javascript basically means the breaking down of a complex structure(Objects or arrays) into simpler parts. With the destructing assignment, we can ‘unpack’ array objects into a bunch of variables.
+
+  // Object Destructuring
+const college = {
+    name : 'DTU',
+    est : '1941',
+    isPvt : false
+};
+let {name, est, isPvt} = college;
+console.log(name, est, isPvt);
+
+// Array Destructuring
+// const arr = ['lionel', 'messi', 'barcelona'];
+// let[value1,value2,value3] = arr;
+// console.log(value1, value2, value3);
